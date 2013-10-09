b0fc572d895ef12ed713b2e7b2fc64c9074adf40
Rodrigo Rodriguez <rorodr@gmail.com>
2013-10-09 16:27:22 -0600

    Fixing an error on deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ role :db,  "108.166.77.118", :primary => true # This is where Rails migrations w
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
 after "deploy:update_code", "deploy:create_symlink"
+before "deploy:assets:precompile", "deploy:create_symlink"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
