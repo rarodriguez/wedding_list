1edd1f9aee0602812c7068d34f5bc947ff7e9bef
Rodrigo Rodriguez <rorodr@gmail.com>
2013-10-08 20:32:28 -0600

    Adding the capfile for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :validate_password
+  
+  def validate_password
+    unless params[:clave].blank?
+      session[:password] = params[:clave]
+    end
+    password = session[:password]
+    unless password.to_s == 'boda0712'
+      redirect_to '/404.html'
+    end
+  end
+  
 end
