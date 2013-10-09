ead0d9e50cc7f26fd95f6fea631a9e4eb1300a9c
Rodrigo Rodriguez <rorodr@gmail.com>
2013-10-09 15:53:11 -0600

    Adding a change on the deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ role :db,  "108.166.77.118", :primary => true # This is where Rails migrations w
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-after "deploy:update_code", "db:symlink"
+after "deploy:update_code", "deploy:symlink"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
